41821021b999a3fcc8ddeda72c02537680d8666a
FGacheru <francisgacheru2001@gmail.com>
2020-10-17 12:51:19 +0300

    add else if for cheese

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // $ (document).ready (function(){
-    function Pizza(type,size,crust,toping)
+    function Pizza(types,size,crust,toping)
 
-    this.type = type;
+    this.types = types;
     this.size = size;
     this.crust = crust;
     this.toping = toping;
@@ -16,6 +16,42 @@ var size = $("#size").val();
 var crust = $("#crust").val();
 var toping = $("#toping").val();
 
-var Pizza = new Pizza (type, size, crust, toping);
+var pizza = new Pizza (type, size, crust, toping);
 
+});
+
+$(document).ready(function(){
+    if(type === cheese && size === large && crust === St.Louis && topping === extraCheese)
+    return (ksh1000)
+    else if(type === cheese && size === medium && crust === stLouis && topping === extraCheese){
+        return(ksh800)
+    }else if(type === cheese && size === small && crust === stLouis){
+        return(ksh600)
+    }else if(type === cheese && size === large && crust === neapolitan){
+        return(ksh950)
+    }else if(type === cheese && size === medium && crust === neapolitan){
+        return(ksh750)
+    }else if(type === cheese && size === small && crust === neapolitan){
+        return(ksh550)
+    }else if(type === cheese && size === large && crust === newYork){
+        return(ksh)
+    }else if(type === cheese && size === medium && crust === newYork){
+        return(ksh)
+    }else if(type === cheese && size === small && crust === newYork){
+        return(ksh)
+    }else if(type === cheese && size === large && crust === stuffedCrust){
+        return(ksh)
+    }else if(type === cheese && size === medium && crust === stuffedCrust){
+        return(ksh)
+    }else if(type === cheese && size === small && crust === stuffedCrust){
+        return(ksh)
+    }
+
+
+    
+    // else if(type === veggie && size === large && crust === stLouis && topping === extraCheese){
+    //     return(ksh900)
+    // }else if(type === veggie && size === medium && crust === stLouis && topping === extraCheese){
+
+    // }
 });

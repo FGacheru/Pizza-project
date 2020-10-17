a705a730274c2d3d3b077ab90a993a50787fe74b
FGacheru <francisgacheru2001@gmail.com>
2020-10-17 09:27:23 +0300

    add a bussiness logic to my jquery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-function Pizza(){
+// $ (document).ready (function(){
+    function Pizza(type,size,crust,toping)
 
-}
+    this.type = type;
+    this.size = size;
+    this.crust = crust;
+    this.toping = toping;
+
+
+
+// });
+$("form#newPizza").submit(function(click){
+event.preventDefault()
+var type = $("#type").val();
+var size = $("#size").val();
+var crust = $("#crust").val();
+var toping = $("#toping").val();
+
+var Pizza = new Pizza (type, size, crust, toping);
+
+});

a96ffbced0e56d19c38128faa59d381e016b9320
FGacheru <francisgacheru2001@gmail.com>
2020-10-19 19:18:19 +0300

    add a location to my form

--- js/script.js
+++ js/script.js
@@ -1,485 +1,148 @@
-
-
-// $(document).ready(function () {
-//     function Pizaa(ytypes, crust, toping, size) {
-//         this.ytypes = ytypes
-//         this.crust = crust;
-//         this.toping = toping;
-//         this.size = size;
-
-       
-//     };
-//     var newOrder = [];
-//     var total = 0;
-
-//     $("#proceed").submit(function (event) {
-//         event.preventDefault();
-//         var ytypes = $("input#ytypes").val();
-//         var crust = $("input#crust").val();
-//         var toping = $("input#toping").val();
-//         var size = $("input#size").val();
-//         var pizza = new Pizaa(ytypes, crust, toping, size)
-
-//         newOrder.push(pizza)
-//         console.log(newOrder)
-//         $("input#ytypes").val("")
-//         $("input#crust").val("");
-//         $("input#toping").val("");
-//         $("input#size").val("");
-//     });
-
-
-
-//     Pizaa.prototype.topingPrice = function () {
-//         if (this.crust === "stLouis") {
-//             if (this.topping === "extraCheese") {
-//                 return 150
-//             } else
-//             if (this.topping === "blackOlives") {
-//                 return 200
-//             } else if (this.topping === "greenPeppers") {
-//                 return 250
-//             } else {
-//                 return 300
-//             }
-//         } else
-//         if (this.crust === "neapolitan") {
-//             if (this.topping === "extraCheese") {
-//                 return 150
-//             } else
-//             if (this.topping === "blackOlives") {
-//                 return 200
-//             } else if (this.topping === "greenPeppers") {
-//                 return 250
-//             } else {
-//                 return 300
-//             }
-//         } else
-//         if (this.crust === "newYork") {
-//             if (this.topping === "extraCheese") {
-//                 return 150
-//             } else
-//             if (this.topping === "blackOlives") {
-//                 return 200
-//             } else if (this.topping === "greenPeppers") {
-//                 return 250
-//             } else {
-//                 return 300
-//             }
-//         } else
-//         if (this.crust === "stuffedCrust") {
-//             if (this.topping === "extraCheese") {
-//                 return 150
-//             } else
-//             if (this.topping === "blackOlives") {
-//                 return 200
-//             } else if (this.topping === "greenPeppers") {
-//                 return 250
-//             } else {
-//                 return 300
-//             }
-//         }
-
-//     }
-
-//     Pizaa.prototype.sizePrize = function () {
-//         if (this.crust === "stLouis") {
-//             if (this.size === "large") {
-//                 return 150
-//             } else
-//             if (this.size === "medium") {
-//                 return 200
-//             } else if (this.size === "small") {
-//                 return 250
-//             } else {
-//                 return 300
-//             }
-//         } else
-//         if (this.crust === "neapolitan") {
-//             if (this.size === "large") {
-//                 return 150
-//             } else
-//             if (this.size === "medium") {
-//                 return 200
-//             } else if (this.size === "small") {
-//                 return 250
-//             } else {
-//                 return 300
-//             }
-//         } else
-//         if (this.crust === "newYork") {
-//             if (this.size === "large") {
-//                 return 150
-//             } else
-//             if (this.size === "medium") {
-//                 return 200
-//             } else if (this.size === "small") {
-//                 return 250
-//             } else {
-//                 return 300
-//             }
-//         } else
-//         if (this.crust === "stuffedCrust") {
-//             if (this.size === "large") {
-//                 return 150
-//             } else
-//             if (this.size === "medium") {
-//                 return 200
-//             } else if (this.size === "small") {
-//                 return 250
-//             } else {
-//                 return 300
-//             }
-//         }
-
-//     }
-
-//     Pizaa.prototype.typePrize = function () {
-//         if (this.crust === "stLouis") {
-//             if (this.size === "cheese") {
-//                 return 150
-//             } else
-//             if (this.size === "veggie") {
-//                 return 200
-//             } else if (this.size === "meat") {
-//                 return 250
-//             } else {
-//                 return 300
-//             }
-//         } else
-//         if (this.crust === "neapolitan") {
-//             if (this.size === "cheese") {
-//                 return 150
-//             } else
-//             if (this.size === "veggie") {
-//                 return 200
-//             } else if (this.size === "meat") {
-//                 return 250
-//             } else {
-//                 return 300
-//             }
-//         } else
-//         if (this.crust === "newYork") {
-//             if (this.size === "cheese") {
-//                 return 150
-//             } else
-//             if (this.size === "veggie") {
-//                 return 200
-//             } else if (this.size === "meat") {
-//                 return 250
-//             } else {
-//                 return 300
-//             }
-//         } else
-//         if (this.crust === "stuffedCrust") {
-//             if (this.size === "cheese") {
-//                 return 150
-//             } else
-//             if (this.size === "veggie") {
-//                 return 200
-//             } else if (this.size === "meat") {
-//                 return 250
-//             } else {
-//                 return 300
-//             }
-//         }
-
-//     }
-
-
-//     Pizaa.prototype.totalPrice = function () {
-//         return this.topingPrice() + this.sizePrize() + this.typePrize()
-        
-//     }
-//     total = newOrder.totalPrice();
-
-// });
-
-
-
+const prices = {
+  flavour: {
+    veggie: 10,
+    meat: 10,
+    cheese: 10,
+  },
+  size: {
+    large: 10,
+    medium: 10,
+    small: 10,
+  },
+  crust: {
+    new_york: 10,
+    st_louis: 10,
+    neapolitan: 10,
+    stuffed_crust: 10,
+  },
+  topping: {
+    extra_cheese: 10,
+    black_olives: 10,
+    green_pepper: 10,
+  },
+};
+const nameMapper = {
+  extra_cheese: "Extra Cheese",
+  black_olives: "Black Olives",
+  green_pepper: "Green Pepper",
+  new_york: "New York",
+  st_louis: "St. Louis",
+  neapolitan: "Neapolitan",
+  stuffed_crust: "Stuffed Crust",
+  veggie: "Veggie",
+  meat: "Meat",
+  cheese: "Cheese",
+  small: "Small",
+  large: "Large",
+  medium: "Medium",
+};
 $(document).ready(function () {
-    function Pizza(ytype, crust, topping, size) {
-        this.ytypes = ytype;
-        this.crust = crust;
-        this.topping = topping;
-        this.size = size;
+  function Pizza(flavour, crust, topping, size, price) {
+    this.flavour = flavour;
+    this.crust = crust;
+    this.topping = topping;
+    this.size = size;
+    this.price = price;
+  }
+  var totalAmount = 0;
+  var newOrder = [];
+
+  $("#proceed").submit(function (event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const flavour = form.get("ytype");
+    const crust = form.get("crust");
+    const topping = form.get("topping");
+    const size = form.get("size");
+    const pizzaPrice =
+      prices.flavour[flavour] +
+      prices.crust[crust] +
+      prices.topping[topping] +
+      prices.size[size];
+    const newPizza = new Pizza(flavour, crust, topping, size, pizzaPrice);
+    newOrder.push(newPizza);
+    const data = `
+   <tr>
+    <td>${nameMapper[newPizza.flavour]}</td>
+    <td>${nameMapper[newPizza.topping]}</td>
+    <td>${nameMapper[newPizza.crust]}</td>
+    <td>${nameMapper[newPizza.size]}</td>
+    <td>${newPizza.price}</td>   
+   </tr>`;
+    $("#order-summary").append(data);
+    totalAmount = newOrder.reduce((pv, cv) => pv + cv.price, 0);
+    $("#finalAmount").empty();
+    $("#finalAmount").append(totalAmount);
+    $(".finalAmount").show();
+  });
+  $("#proceed").click(function () {
+    $(".checkout-options").show();
+  });
+
+  $("#checkout-form").submit(function (event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    var deliveryOption = $("#delivery-option").val();
+    customerName = name;
+    $("#name").val("");
+    $("#delivery-option").val("");
+    $(".checkout-options").hide();
+    $("#proceed").hide();
+    $("#finalAmount").empty();
+    if (deliveryOption === "deliver") {
+      $(".location").show();
+      $(".delivery-cost").show();
+      $("#finalAmount").empty();
+      $("#delivery-amount").append(300);
+      totalAmount += 300;
+      $("#finalAmount").empty();
+      // $("#finalAmount").empty();
+      $("#finalAmount").append(totalAmount);
+      $(".finalAmount").show();
+
+      // $("#finalAmount").append(totalAmount);
+    } else {
+      alert(
+        customerName +
+          ": Your total bill is Ksh. " +
+          totalAmount +
+          ". Your order will be ready for collection in the next one hours"
+      );
     }
-    var totalAmount = 0;
-    var newOrder = [];
+  });
 
-
-    $("#proceed").submit(function (event) {
-        event.preventDefault();
-        var ytype = $("#ytype").val();
-        var crust = $("#crust").val();
-        var size = $("#size").val();
-        var topping = $("#topping").val();
-        var newPizza = new Pizza(ytype,crust,topping,size);
-        newOrder.push(newPizza);
-        
-        $("#ytype").val("");
-        $("#crust").val("");
-        $("#size").val("");
-        $("#topping").val("");
-
-
-    Pizza.prototype.toppingPrize = function () {
-        Pizaa.prototype.topingPrice = function () {
-            if (this.crust === "stLouis") {
-                if (this.topping === "extraCheese") {
-                    return 150
-                } else
-                if (this.topping === "blackOlives") {
-                    return 200
-                } else if (this.topping === "greenPeppers") {
-                    return 250
-                } else {
-                    return 300
-                }
-            } else
-            if (this.crust === "neapolitan") {
-                if (this.topping === "extraCheese") {
-                    return 150
-                } else
-                if (this.topping === "blackOlives") {
-                    return 200
-                } else if (this.topping === "greenPeppers") {
-                    return 250
-                } else {
-                    return 300
-                }
-            } else
-            if (this.crust === "newYork") {
-                if (this.topping === "extraCheese") {
-                    return 150
-                } else
-                if (this.topping === "blackOlives") {
-                    return 200
-                } else if (this.topping === "greenPeppers") {
-                    return 250
-                } else {
-                    return 300
-                }
-            } else
-            if (this.crust === "stuffedCrust") {
-                if (this.topping === "extraCheese") {
-                    return 150
-                } else
-                if (this.topping === "blackOlives") {
-                    return 200
-                } else if (this.topping === "greenPeppers") {
-                    return 250
-                } else {
-                    return 300
-                }
-            }
-    
-        }
-    
-        Pizaa.prototype.sizePrize = function () {
-            if (this.crust === "stLouis") {
-                if (this.size === "large") {
-                    return 150
-                } else
-                if (this.size === "medium") {
-                    return 200
-                } else if (this.size === "small") {
-                    return 250
-                } else {
-                    return 300
-                }
-            } else
-            if (this.crust === "neapolitan") {
-                if (this.size === "large") {
-                    return 150
-                } else
-                if (this.size === "medium") {
-                    return 200
-                } else if (this.size === "small") {
-                    return 250
-                } else {
-                    return 300
-                }
-            } else
-            if (this.crust === "newYork") {
-                if (this.size === "large") {
-                    return 150
-                } else
-                if (this.size === "medium") {
-                    return 200
-                } else if (this.size === "small") {
-                    return 250
-                } else {
-                    return 300
-                }
-            } else
-            if (this.crust === "stuffedCrust") {
-                if (this.size === "large") {
-                    return 150
-                } else
-                if (this.size === "medium") {
-                    return 200
-                } else if (this.size === "small") {
-                    return 250
-                } else {
-                    return 300
-                }
-            }
-    
-        }
-    
-        Pizaa.prototype.typePrize = function () {
-            if (this.crust === "stLouis") {
-                if (this.size === "cheese") {
-                    return 150
-                } else
-                if (this.size === "veggie") {
-                    return 200
-                } else if (this.size === "meat") {
-                    return 250
-                } else {
-                    return 300
-                }
-            } else
-            if (this.crust === "neapolitan") {
-                if (this.size === "cheese") {
-                    return 150
-                } else
-                if (this.size === "veggie") {
-                    return 200
-                } else if (this.size === "meat") {
-                    return 250
-                } else {
-                    return 300
-                }
-            } else
-            if (this.crust === "newYork") {
-                if (this.size === "cheese") {
-                    return 150
-                } else
-                if (this.size === "veggie") {
-                    return 200
-                } else if (this.size === "meat") {
-                    return 250
-                } else {
-                    return 300
-                }
-            } else
-            if (this.crust === "stuffedCrust") {
-                if (this.size === "cheese") {
-                    return 150
-                } else
-                if (this.size === "veggie") {
-                    return 200
-                } else if (this.size === "meat") {
-                    return 250
-                } else {
-                    return 300
-                }
-            }
-        }
-        }
-    
-        Pizaa.prototype.totalPrice = function () {
-            return this.toppingPrize() + this.sizePrize() + this.typePrize()
-    }
-    
-
-    
-
-
-        for (let i = 0; i < newOrder.length; i++) {
-            totalAmount += newOrder[i].pizzaPrice();
-        }
-        $("#proceed").append(
-            "<tr>" +
-            '<th scope="row">' +
-            newPizza.type +
-            " - " +
-            newPizza.size +
-            "  @ " +
-            newPizza.typePrize() +
-            "</th>" +
-            "<td>" +
-            newPizza.toppings +
-            " @ " +
-            newPizza.toppingPrize() +
-            "</td>" +
-            "<td>" +
-            newPizza.crust +
-            " @ " +
-            newPizza.crustPrize() +
-            "</td>" +
-            "<td>" +
-            newPizza.totalPrice() +
-            "</td>" +
-            "</tr>"
-        );
-        $("#proceed").append("");
-        if (newOrder.length > 0) {
-            $("#form-title").empty();
-            $("#form-title").append("Add Another Order");
-        }
-
-        $("#finalAmount").fadeIn();
-        $("#proceed").fadeIn();
-        $("#orders-div").fadeIn();
-
-        $("#finalAmount").empty();
-        $("#finalAmount").append(totalAmount);
-        $(".finalAmount").show();
-    });
-    $("#checkout").click(function () {
-        $(".checkout-options").show();
-    });
-
-    $("#checkout-form").submit(function (event) {
-        event.preventDefault();
-        var name = $("#name").val();
-        var deliveryOption = $("#delivery-option").val();
-        customerName = name;
-        $("#name").val("");
-        $("#delivery-option").val("");
-        $(".checkout-options").hide();
-        $("#checkout").hide();
-        $("#finalAmount").empty();
-        if (deliveryOption === "deliver") {
-            $(".location").show();
-            $(".delivery-cost").show();
-            $("#finalAmount").empty();
-            $("#delivery-amount").append(300);
-            totalAmount += 300;
-            $("#finalAmount").empty();
-            $("#finalAmount").empty();
-        $("#finalAmount").append(totalAmount);
-        $(".finalAmount").show();
-
-            $("#finalAmount").append(totalAmount);
-        } else {
-            alert(customerName + ": Your total bill is Ksh. " + totalAmount + ". Your order will be ready for collection in the next one hours");
-        }
-    });
-
-    $("#location-form").submit(function (event) {
-        event.preventDefault();
-        var estateEntered = $("#estate").val();
-        var houseNumberEntered = $("#house-number").val();
-        estate = estateEntered;
-        houseNumber = houseNumberEntered;
-        $("#checkout").hide();
-        $(".location").hide();
-        $("#finalAmount").empty();
-        $("#finalAmount").append(totalAmount);
-        $(".finalAmount").show();
-        alert(customerName + ": Your total bill is Ksh. " + totalAmount + ". Your order will be delivered to " + estate + ", " + houseNumber + " in the next one hours");
-    });
-    $("#contacts").submit(function (event) {
-        event.preventDefault();
-        var name1 = $("#form1").val();
-        var email1 = $("#form2").val();
-        var message1 = $("#form3").val();
-        alert("Hello! " + name1 + "," + " We have received your message. Thank you for reaching out to us. ");
-
-    });
-    
- });
+  $("#location-form").submit(function (event) {
+    event.preventDefault();
+    var estateEntered = $("#estate").val();
+    var houseNumberEntered = $("#house-number").val();
+    estate = estateEntered;
+    houseNumber = houseNumberEntered;
+    $("#proceed").hide();
+    $(".location").hide();
+    $("#finalAmount").empty();
+    $("#finalAmount").append(totalAmount);
+    $(".finalAmount").show();
+    alert(
+      customerName +
+        ": Your total bill is Ksh. " +
+        totalAmount +
+        ". Your order will be delivered to " +
+        estate +
+        ", " +
+        houseNumber +
+        " in the next one hour"
+    );
+  });
+  $("#contacts").submit(function (event) {
+    event.preventDefault();
+    var name1 = $("#form1").val();
+    var email1 = $("#form2").val();
+    var message1 = $("#form3").val();
+    alert(
+      "Hello! " +
+        name1 +
+        "," +
+        " We have received your message. Thank you for reaching out to us. "
+    );
+  });
+});
